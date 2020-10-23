fad6f788c05f459ef8ed8b95a8adee3a7f28e1f4
abu_sufian <abusufianshaon@gmail.com>
2020-10-23 16:50:06 +0600

    First Commit

---
+++ app/src/main/java/com/abu/sufian/toastapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.abu.sufian.toastapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtToastText;
+    private Button btnSubmitToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtToastText = findViewById(R.id.edtToastText);
+        btnSubmitToast = findViewById(R.id.btnSubmitToast);
+
+        btnSubmitToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = edtToastText.getText().toString();
+                if(!TextUtils.isEmpty(text)){
+                    Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
